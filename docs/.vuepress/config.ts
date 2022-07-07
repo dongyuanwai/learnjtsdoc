a6a49d0a56abd4f30748873568f10145e48fbf8c
dongyuanwai <yuanwaidong@gmail.com>
2022-07-07 23:23:46 +0800

    顶部导航条

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,4 @@
-import { defineUserConfig, } from 'vuepress'
+import { defineUserConfig, defaultTheme} from 'vuepress'
 
 export default defineUserConfig({
 	base:'/',
@@ -7,4 +7,39 @@ export default defineUserConfig({
 	description: 'JS工具类方法,手写常用js方法汇总',
 	head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
 
+    theme: defaultTheme({
+		// tab栏的图标; 图片 / 会自动去public文件夹里找图片
+		logo: '/images/logo.png',
+
+		// 顶部导航条   
+		navbar: [
+			{
+				text: '介绍',
+				link: '/pages/introduce.md',
+			},
+			// NavbarGroup
+			{
+				text: '教程',
+				children: [
+					{
+						text: '安装指南',
+						link: '/pages/learnJTs/install_guide.md',
+						// 该元素将一直处于激活状态
+						activeMatch: '/pages/learnJTs/install_guide.md',
+					},
+					{
+						text: 'API使用',
+						link: '/pages/learnJTs/detail_usage.md',
+							activeMatch: '/pages/learnJTs/detail_usage.md',
+						},
+					{
+						text: '待定...',
+						link: '/pages/other/other.md',
+						},
+					],
+			},
+		],
+		repo: 'https://github.com/dongyuanwai/learnjts',
+	}),
+    
 })
