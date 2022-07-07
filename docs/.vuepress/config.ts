6d06c0ff8e8b475c683c3aa5391750f8e0f13952
dongyuanwai <yuanwaidong@gmail.com>
2022-07-07 22:53:27 +0800

    安装vuepress进行基本配置

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import { defineUserConfig, } from 'vuepress'
+
+export default defineUserConfig({
+	base:'/',
+	lang: 'zh-CN',
+	title: 'LearnJTs',
+	description: 'JS工具类方法,手写常用js方法汇总',
+	head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+
+})
